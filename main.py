073cc6773ad12450fdfc8b980d560ad4b339ffb1
smmih <s.m.mih@mail.ru>
2025-05-07 23:13:13 +0100

    Added: \u00A0

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pyperclip
 def find_narrow_spaces(filename):
     narrow_space = '\u202F'
     em_dash = '\u2014'
+    nbsp = '\u00A0'
+
     with open(filename, 'r', encoding='utf-8') as f:
         lines = f.readlines()
 
@@ -22,9 +24,15 @@ def find_narrow_spaces(filename):
             positions = [str(pos) for pos, char in enumerate(line) if char == em_dash]
             print(", ".join(positions))
             found = True
+        if nbsp in line:
+            print(f"Строка {i}: {line.strip()}")
+            print(f"→ Найден неразрывный пробел (U+AO) на позиции: ", end='')
+            positions = [str(pos) for pos, char in enumerate(line) if char == em_dash]
+            print(", ".join(positions))
+            found = True
 
     if not found:
-        print("Узкий пробел или длинное тире в файле не найдены.")
+        print("Узкий пробел или длинное тире или неразрывный пробел в файле не найдены.")
 
 
 if __name__ == "__main__":
