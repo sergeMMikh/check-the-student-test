553c0635205b0cc7ec5fa97ec05ba32f530745db
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2026-01-02 15:50:11 +0000

    added: counts

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def find_narrow_spaces(filename):
     with open(filename, 'r', encoding='utf-8') as f:
         lines = f.readlines()
 
+    count = [0,0,0]
+
     found = False
     for i, line in enumerate(lines, start=1):
         if narrow_space in line:
@@ -18,21 +20,26 @@ def find_narrow_spaces(filename):
             positions = [str(pos) for pos, char in enumerate(line) if char == narrow_space]
             print(", ".join(positions))
             found = True
+            count[0] += 1
         if em_dash in line:
             print(f"Строка {i}: {line.strip()}")
             print(f"→ Найден длинное тире (U+2014) на позиции: ", end='')
             positions = [str(pos) for pos, char in enumerate(line) if char == em_dash]
             print(", ".join(positions))
             found = True
+            count[1] += 1
         if nbsp in line:
             print(f"Строка {i}: {line.strip()}")
             print(f"→ Найден неразрывный пробел (U+AO) на позиции: ", end='')
             positions = [str(pos) for pos, char in enumerate(line) if char == em_dash]
             print(", ".join(positions))
             found = True
+            count[2] += 1
 
     if not found:
         print("Узкий пробел или длинное тире или неразрывный пробел в файле не найдены.")
+    else:
+        print(f'Найдено:\n\tузкий пробел: {count[0]}\n\tдлинное тире: {count[1]}\n\tнеразрывный пробел: {count[2]}')
 
 
 if __name__ == "__main__":
