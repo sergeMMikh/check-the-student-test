d37e2be49803f37f480a6fffdd84a4d95a0f0865
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2026-01-20 10:29:15 +0000

    added: arrow

--- main.py
+++ main.py
@@ -3,43 +3,61 @@ import pyperclip
 
 
 def find_narrow_spaces(filename):
-    narrow_space = '\u202F'
-    em_dash = '\u2014'
-    nbsp = '\u00A0'
+    narrow_space = '\u202F'   # узкий пробел
+    em_dash = '\u2014'        # —
+    nbsp = '\u00A0'           # неразрывный пробел
+    arrow = '\u2192'          # →
 
     with open(filename, 'r', encoding='utf-8') as f:
         lines = f.readlines()
-
-    count = [0, 0, 0]
+        
+    count = [0, 0, 0, 0]
 
     found = False
     for i, line in enumerate(lines, start=1):
         if narrow_space in line:
             print(f"Строка {i}: {line.strip()}")
-            print(f"→ Найден узкий пробел (U+202F) на позиции: ", end='')
+            print("→ Найден узкий пробел (U+202F) на позиции: ", end='')
             positions = [str(pos) for pos, char in enumerate(line) if char == narrow_space]
             print(", ".join(positions))
             found = True
-            count[0] += 1
+            count[0] += len(positions)
+
         if em_dash in line:
             print(f"Строка {i}: {line.strip()}")
-            print(f"→ Найден длинное тире (U+2014) на позиции: ", end='')
+            print("→ Найдено длинное тире (U+2014) на позиции: ", end='')
             positions = [str(pos) for pos, char in enumerate(line) if char == em_dash]
             print(", ".join(positions))
             found = True
-            count[1] += 1
+            count[1] += len(positions)
+
         if nbsp in line:
             print(f"Строка {i}: {line.strip()}")
-            print(f"→ Найден неразрывный пробел (U+AO) на позиции: ", end='')
+            print("→ Найден неразрывный пробел (U+00A0) на позиции: ", end='')
             positions = [str(pos) for pos, char in enumerate(line) if char == nbsp]
             print(", ".join(positions))
             found = True
-            count[2] += 1
+            count[2] += len(positions)
+
+        if arrow in line:
+            print(f"Строка {i}: {line.strip()}")
+            print("→ Найдена стрелка (U+2192) на позиции: ", end='')
+            positions = [str(pos) for pos, char in enumerate(line) if char == arrow]
+            print(", ".join(positions))
+            found = True
+            count[3] += len(positions)
 
     if not found:
-        print("Узкий пробел или длинное тире или неразрывный пробел в файле не найдены.")
+        print("Специальные символы в файле не найдены.")
     else:
-        print(f'Найдено:\n\tузкий пробел: {count[0]}\n\tдлинное тире: {count[1]}\n\tнеразрывный пробел: {count[2]}')
+        print(
+            "Найдено:\n"
+            f"\tузкий пробел: {count[0]}\n"
+            f"\tдлинное тире: {count[1]}\n"
+            f"\tнеразрывный пробел: {count[2]}\n"
+            f"\tстрелка →: {count[3]}"
+        )
+
 
 
 if __name__ == "__main__":
